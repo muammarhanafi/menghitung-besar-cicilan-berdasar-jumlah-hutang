d886f0782a7918a53ee2f3f4a3e3bfceeb320d05
muammarhanafi <135295367+muammarhanafi@users.noreply.github.com>
2023-06-02 13:56:56 +0700

    Tugas individu 287

---
+++ individu_2113010287.java
@@ -0,0 +1,13 @@
+public class individu_2113010287 {
+    public static void main(String[] args) {
+        String nama = "MuammarHanafi";
+        String nim = "2113010287";
+        String kelas = "4 G";
+        String alamat = "sawit";
+
+        System.out.println("Name: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("Alamat: " + alamat);
+    }
+}
