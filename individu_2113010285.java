9b0de69cec89f6da6486ec05044fb2240e6048ed
Angls900 <134506980+Angls900@users.noreply.github.com>
2023-06-02 17:59:36 +0700

    tugas individu 285

---
+++ individu_2113010285.java
@@ -0,0 +1,13 @@
+public class individu_2113010285 {
+    public static void main(String[] args) {
+        String nama = "Fadhil Moreno E.N";
+        String nim = "2113010285";
+        String kelas = "4 G";
+        String alamat = "Boyolali";
+
+        System.out.println("Name: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("Alamat: " + alamat);
+    }
+}
